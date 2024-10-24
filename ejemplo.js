9b4c9a487fb1187202a92afc76df6660049a6d08
1-ERICK <valdiviesolobatone@gmail.com>
2024-10-24 12:17:11 -0500

    mi primer commit

---
+++ ejemplo.js
@@ -0,0 +1,41 @@
+// VARIABLES
+//var ejemplo
+// var a = 1
+// var a = 2
+// console.log(a)
+//let
+// let b = 1
+//const
+// const pi = 3.1446
+// const meses =12
+
+// + - / % *
+
+// let c = 9
+// let d = 2
+
+// let res 
+
+// res = c % d
+
+// console.log("el residuo es :", res)
+
+let lado = 5
+let res
+res = lado * lado 
+console.log("el area del cuadrado es :", res)
+console.log("-------------------------------------")
+
+let b =10
+let a =5
+let x
+x = (b*a)
+console.log("el area del rectangulo es :", x)
+console.log("-------------------------------------")
+
+let base = 5
+let altura = 10
+let p
+p = (base * altura)/2
+console.log("el are del triangulo es :", p)
+console.log("-------------------------------------")
